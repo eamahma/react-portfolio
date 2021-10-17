f6f18173d3ffe793f0cba86ded3626b5cc5dc8a4
eamahma <eamahma@gmail.com>
2021-10-17 10:53:50 -0400

    adding Header and About components

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Amir Ahmadi</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/u/30188334?v=4" alt="Amir Ahmadi" />
+
+                <p>
+                Electrical Engineer and Wireless telecommunication professional; passionate about coding, hardware, firmware, and IoT.
+                </p>
+                <p>
+                Highly accomplished Wireless Network and Telecommunication professional with experience in Radio Cell Planning, Optimization, and Network Engineering. Member of Professional Engineers and Geoscientists of British Columbia (P.Eng.). 
+                </p>
+                <p>
+                Hands-on cellular networks expertise includes cellular network design for Macro, Micro, in-building DAS and Small Cell, link budget analysis, site selection, RF Safety.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
