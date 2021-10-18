ed90a53e8917497ee5b9c6feb376546e6103b69c
eamahma <eamahma@gmail.com>
2021-10-17 20:11:10 -0400

    project cards formatted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,12 @@ import {FaTwitter, FaLinkedin, FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <footer className="w-100 mt-auto bg-primary p-4">
+    <footer className="w-100 mt-auto p-4">
       <div className="container text-white text-center mb-5">
         <ul class="list-inline  text-center">
-          <li class="list-inline-item fa-2x"><a href="https://github.com/eamahma"><FaGithub /></a></li>
-          <li class="list-inline-item fa-2x"><a href="https://www.linkedin.com/in/amir-ahmadi-858110190/"><FaLinkedin /></a></li>
-          <li class="list-inline-item fa-5x"><a href="https://twitter.com/aammiirr"><FaTwitter /></a></li>
-          <li class="list-inline-item fa-5x"><a href="https://twitter.com/aammiirr"><img src="./assets/linkedin.png" alt="twitter"/></a></li>
+          <li class="list-inline-item fa-2x"><h4><a href="https://github.com/eamahma"><FaGithub /></a></h4></li>
+          <li class="list-inline-item fa-2x"><h4><a href="https://www.linkedin.com/in/amir-ahmadi-858110190/"><FaLinkedin /></a></h4></li>
+          <li class="list-inline-item fa-5x"><h4><a href="https://twitter.com/aammiirr"><FaTwitter /></a></h4></li>
         </ul>
       </div>
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,38 +1,17 @@
 
 import React from 'react'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {FaGithub} from 'react-icons/fa'
 
 // Individual Cards 
-// function ProjectCards(props) {
-//     return (
-//       <div className="card"> 
-//         <div className="img-container">
-//           <img alt={props.name} src={props.image}/>
-//         </div>
-//         <div className="content">
-//               <p className="card-title">{props.name}</p>
-//               <p class="proj-icons-container">
-//               <a href={props.github}><img class="project-icon" src="https://img.icons8.com/fluent/48/000000/github.png" alt="GitHub Repo" /></a> 
-//               <a href={props.deploy}><img class="project-icon" src="https://img.icons8.com/fluent/48/000000/domain.png" alt="Live Application" /></a> 
-       
-             
-//              </p>
-//               <p class="topics">
-//                     ({props.topics})
-//               </p>
-//         </div>
-//       </div>
-//     );
-// }
 
 function ProjectCards(props) {
     return (
       <div class="card bg-light text-dark">
         <img class="card-img" src={props.image} alt={props.name}/>
         <div class="card-img-overlay">
-          <h5 class="card-title">{props.name}</h5>
-          <p class="card-text">{props.github}.</p>
-          <p class="card-text">{props.deploy}</p>
+          <a href={props.deploy}><h4 class="card-title">{props.name}</h4></a>
+          <a href={props.github}><h4 class="card-icon"><FaGithub /></h4></a>
         </div>
       </div>
     );
